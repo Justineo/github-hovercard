71152102862c7c40da2f7eed7fc4c75f726b5c7e
Justineo <justice360@gmail.com>
2016-01-18 11:33:23 +0800

    fix updateURL

--- userscript/src/metadata.js
+++ userscript/src/metadata.js
@@ -3,8 +3,9 @@
 // @namespace https://justineo.github.io/
 // @version 0.3.6
 // @description Neat user/repo/issue hovercard for GitHub.
+// @author Justineo(justice360@gmail.com)
 // @match https://github.com/*
 // @grant none
-// @updateURL https://justineo.github.io/github-hovercard/userscript/dist/userscript.js
+// @updateURL https://justineo.github.io/github-hovercard/userscript/dist/github-hovercard.user.js
 // @icon https://justineo.github.io/github-hovercard/icon.png
 // ==/UserScript==
