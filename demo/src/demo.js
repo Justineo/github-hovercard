09d2509ef743168dbe967d6821b0b5231b18a91f
Justineo <justice360@gmail.com>
2016-02-05 10:54:53 +0800

    add Opera inline installation

--- demo/src/demo.js
+++ demo/src/demo.js
@@ -11,12 +11,12 @@ $(() => {
   const EXT_ID = {
     chrome: 'mmoahbbnojgkclgceahhakhnccimnplk',
     mozilla: '641356',
-    opera: ''
+    opera: 'github-hovercard'
   }[browser];
   const VENDOR_URL = {
     chrome: `https://chrome.google.com/webstore/detail/${EXT_ID}`,
-    mozilla: 'https://addons.mozilla.org/en-US/firefox/addon/github-hovercard/',
-    opera: ''
+    mozilla: `https://addons.mozilla.org/en-US/firefox/addon/${EXT_ID}/`,
+    opera: `https://addons.opera.com/extensions/details/${EXT_ID}/`
   }[browser];
   const VENDOR_NAME = {
     chrome: 'Chrome Webstore',
@@ -28,9 +28,12 @@ $(() => {
     if (browser === 'chrome') {
       setInstalling();
       chrome.webstore.install(VENDOR_URL, setInstalled.bind(null, true), reset);
+    } else if (browser === 'opera') {
+      setInstalling();
+      opr.addons.installExtension(EXT_ID, setInstalled.bind(null, true), reset);
     } else if (browser === 'mozilla') {
       let result = InstallTrigger.install({
-        "GitHub Hovercard": {
+        'GitHub Hovercard': {
           URL: 'https://addons.mozilla.org/firefox/downloads/latest/' + EXT_ID + '/addon-' + EXT_ID + '-latest.xpi',
           ICON_URL: 'https://addons.cdn.mozilla.net/user-media/addon_icons/641/' + EXT_ID + '.png?modified=1450363198'
         }
@@ -42,13 +45,18 @@ $(() => {
 
   function checkInstalled() {
     switch (browser) {
-      case 'chrome':
-      case 'opera': {
+      case 'chrome': {
         chrome.runtime.sendMessage(EXT_ID, { message: 'version' }, (resp) => {
           if (resp) {
             setInstalled();
           }
         });
+        break;
+      }
+      case 'opera': {
+        // chrome.runtime is undefined, not as the docs on Dev.Opera described.
+        // So we do nothing here for now.
+        break;
       }
       case 'mozilla': {
         let timer = setInterval(() => {
@@ -57,6 +65,7 @@ $(() => {
             clearTimeout(timer);
           }
         }, 100);
+        break;
       }
     }
   }
@@ -71,8 +80,8 @@ $(() => {
     $installBtn.text('Installing...').prop('disabled', true);
   }
 
-  function reset() {
+  function reset(msg) {
     $installBtn.text('Install').prop('disabled', false);
-    $hint.html(`Something went wrong. Try again or download at <a href="${VENDOR_URL}">${VENDOR_NAME}</a>.`);
+    $hint.html(`${msg || 'Something went wrong.'} Try again or download at <a href="${VENDOR_URL}">${VENDOR_NAME}</a>.`);
   }
 });
