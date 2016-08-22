a60b4ffb5731108424eacef2921f9751a5b5f281
Justineo <justice360@gmail.com>
2016-08-22 11:07:42 +0800

    update version

--- userscript/dist/github-hovercard.user.js
+++ userscript/dist/github-hovercard.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name GitHub Hovercard
 // @namespace https://justineo.github.io/
-// @version 0.7.1
+// @version 0.7.2
 // @description Neat user/repo/issue hovercards for GitHub.
 // @author Justineo(justice360@gmail.com)
 // @match https://github.com/*
