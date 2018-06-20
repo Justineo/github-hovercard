ddc806f1938c1d35a38c81f07f5c85c64174a51c
Justineo <justice360@gmail.com>
2018-06-20 16:30:46 +0800

    chore: bump version

--- userscript/dist/github-hovercard.user.js
+++ userscript/dist/github-hovercard.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name GitHub Hovercard
 // @namespace https://justineo.github.io/
-// @version 1.4.5
+// @version 1.5.0
 // @description Neat user/repo/issue hovercards for GitHub.
 // @author Justineo(justice360@gmail.com)
 // @match https://github.com/*
